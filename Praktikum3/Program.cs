96133636c88c6af28a33216a4edbe217b62ab23d
lincchi <caroline.lincchi@gmail.com>
2020-03-22 11:26:46 +0700

    Class Taxi

---
+++ Praktikum3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Zidan";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Praktikum3/Taxi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum3
+{
+    public class Taxi
+    {
+        //properties 
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name :{0}", DriverName);
+
+            if(OnDuty)
+            {
+                Console.WriteLine("On Duty : YES");
+            }
+            else
+            {
+                Console.WriteLine("OnDuty : NO");
+            }
+
+            Console.WriteLine("Number Passenger : {0}", NumPassenger);
+        }
+
+
+        public void PickUpPassenger()
+        {
+            if(OnDuty)
+            {
+                Console.WriteLine("Driver {0} sedang menjemput penumpang", DriverName);
+            }
+            else
+            {
+                Console.WriteLine("Driver {0} tidak sedang menjemput penumpang", DriverName);
+            }
+        }
+
+
+        public void DropOffPassenger()
+        {
+            if (OnDuty)
+            {
+                Console.WriteLine("Driver {0} selesai mengantar penumpang", DriverName);
+            }
+            else
+            {
+                Console.WriteLine("Driver {0}", DriverName);
+            }
+        }
+
+    }
+}
